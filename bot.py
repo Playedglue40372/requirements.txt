1e622837d0b63663b8aa5e05bec3d2bcbc5b3ac7
Playedglue40372 <151623207+Playedglue40372@users.noreply.github.com>
2026-05-16 19:33:55 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,11 +6,13 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 # ====== ЧТЕНИЕ ПЕРЕМЕННЫХ ИЗ ПАНЕЛИ RENDER ======
-API_ID = int(os.getenv("33694427", "0"))
-API_HASH = os.getenv("f819241c056f6827cc0188ff1479a7ce", "")
-BOT_TOKEN = os.getenv("8822781449:AAHiz6Ufn2ju045iDs2YIhcsr4XAAM9urxE", "")
+# Код берет настройки из полей Environment Variables, которые вы заполнили на Render
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# Принудительно указываем искать сессии в текущей папке проекта (workdir=".")
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, workdir=".")
 
 
 def keyboard():
@@ -23,7 +25,8 @@ def keyboard():
 
 
 async def get_proxy():
-    user = Client("proxy_user", api_id=API_ID, api_hash=API_HASH)
+    # Подключаем юзербота, используя загруженный файл proxy_user.session
+    user = Client("proxy_user", api_id=API_ID, api_hash=API_HASH, workdir=".")
 
     await user.start()
 
